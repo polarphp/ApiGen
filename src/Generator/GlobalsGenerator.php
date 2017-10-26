80a99558917d5cdd10a02153042e76f1e1ff908c
softboy <zzu_softboy@163.com>
2017-10-26 10:59:57 +0800

    finished globals pages

--- src/Generator/GlobalsGenerator.php
+++ src/Generator/GlobalsGenerator.php
@@ -5,6 +5,7 @@ namespace ApiGen\Generator;
 use ApiGen\Configuration\Configuration;
 use ApiGen\Contract\Generator\GeneratorInterface;
 use ApiGen\Templating\TemplateRenderer;
+use ApiGen\Element\ReflectionCollector\NamespaceReflectionCollector;
 
 final class GlobalsGenerator implements GeneratorInterface
 {
@@ -17,20 +18,41 @@ final class GlobalsGenerator implements GeneratorInterface
      * @var TemplateRenderer
      */
     private $templateRenderer;
+    /**
+     * @var NamespaceReflectionCollector
+     */
+    private $namespaceReflectionCollector;
 
-    public function __construct(Configuration $configuration, TemplateRenderer $templateRenderer)
+    public function __construct(
+        NamespaceReflectionCollector $namespaceReflectionCollector,
+        Configuration $configuration,
+        TemplateRenderer $templateRenderer
+    )
     {
+        $this->namespaceReflectionCollector = $namespaceReflectionCollector;
         $this->configuration = $configuration;
         $this->templateRenderer = $templateRenderer;
     }
 
     public function generate(): void
     {
+        $nsDir = getcwd().DIRECTORY_SEPARATOR."ConstDefs";
+        $nsDir = str_replace("\\", DIRECTORY_SEPARATOR, $nsDir);
+        $filename = $nsDir.DIRECTORY_SEPARATOR."ConstDefinitions.php";
+        $constants = [];
+        if (file_exists($filename)) {
+            $constants = include $filename;
+        }
         $this->templateRenderer->renderToFile(
             $this->configuration->getTemplateByName('globals'),
             $this->configuration->getDestinationWithName('globals'),
             [
                 'activePage' => 'globals',
+                'classes' => $this->namespaceReflectionCollector->getClassReflections("none"),
+                'interfaces' => $this->namespaceReflectionCollector->getInterfaceReflections("none"),
+                'traits' => $this->namespaceReflectionCollector->getTraitReflections("none"),
+                'functions' => $this->namespaceReflectionCollector->getFunctionReflections("none"),
+                "constants" => $constants,
                 'siteCategory' => $this->configuration->getOption('sitecategory'),
                 'apiCatalog'=> $this->configuration->getOption('apicatalog')
             ]
